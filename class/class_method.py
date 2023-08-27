b95ddb8f321f9e83501535d382e253b61be754b0
lhjwork <jin.come.up.business@gmail.com>
2023-08-27 22:07:38 +0900

    method 활용

---
+++ class/class_method.py
@@ -0,0 +1,26 @@
+class AttackUnit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+
+    # location 은 전달 받은 인자를 받아서 사용
+    def attack(self, location):
+        print(
+            "{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(
+                self.name, location, self.damage
+            )
+        )
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파과되었습니다.".format(self.name))
+
+
+firebat = AttackUnit("파이어뱃", 50, 16)
+firebat.attack("5시")
+firebat.damaged(25)
+firebat.damaged(25)
